69cbff418d06329af0fa9c4e19d2a3411c8e3eda
Sofia183 <152977727+Sofia183@users.noreply.github.com>
2025-04-17 20:14:10 +0200

    ejercicio18.js

--- ejercicios/ejercicio18.js
+++ ejercicios/ejercicio18.js
@@ -1,1 +1,19 @@
+const placesToTravel = [
+  { id: 5, name: 'Japan' },
+  { id: 11, name: 'Venecia' },
+  { id: 23, name: 'Murcia' },
+  { id: 40, name: 'Santander' },
+  { id: 44, name: 'Filipinas' },
+  { id: 59, name: 'Madagascar' }
+];
 
+// Usamos un bucle for para recorrer el array y eliminar los elementos con id 11 y 40
+for (let i = 0; i < placesToTravel.length; i++) {
+  if (placesToTravel[i].id === 11 || placesToTravel[i].id === 40) {
+    placesToTravel.splice(i, 1);
+    i--; // Importante: Reducimos el índice porque el array se acorta
+  }
+}
+
+// Mostramos el array actualizado
+console.log(placesToTravel);
